cc580be69cd51ba891a8fc9773e32b6a5101c252
minaripenguin <minaripenguin@users.noreply.github.com>
2024-02-02 12:33:16 +0900

    SystemUI: IslandView: Implement now playing for Island notification
    neobuddy89: Added few clean ups.
    
    Change-Id: Ic56f44fded8ee3f521738da36968e8aecfde7a95
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7269,6 +7269,11 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION = "island_notification";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
+
+        /**
          * Whether the phone ringtone should be played in an increasing manner
          * 0 = 0ff, 1 = on
          * @hide
