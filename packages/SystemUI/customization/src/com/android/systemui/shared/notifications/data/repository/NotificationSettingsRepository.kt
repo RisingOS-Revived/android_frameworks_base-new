Project-Mist-OS/frameworks-base-lineage

SystemUI: Introduce Notification Peek Display feature [1/2]
* inspired from motorola lockscreen bubble style and android 15/16 beta minimalism feature

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/customization/src/com/android/systemui/shared/notifications/data/repository/NotificationSettingsRepository.kt
@@ -21,6 +21,7 @@ import com.android.systemui.shared.settings.data.repository.SecureSettingsReposi
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
@@ -41,23 +42,42 @@ class NotificationSettingsRepository(
             .distinctUntilChanged()
 
     /** The current state of the notification setting. */
-    suspend fun isShowNotificationsOnLockScreenEnabled(): StateFlow<Boolean> =
-        secureSettingsRepository
-            .intSetting(
-                name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
-            )
-            .map { it == 1 }
-            .flowOn(backgroundDispatcher)
-            .stateIn(
-                scope = scope,
-            )
+    suspend fun isShowNotificationsOnLockScreenEnabled(): StateFlow<Boolean> {
+        val peekDisplayNotification = secureSettingsRepository.getInt(
+            name = "peek_display_notifications",
+            defaultValue = 0
+        )
+
+        return if (peekDisplayNotification == 1) {
+            MutableStateFlow(false)
+        } else {
+            secureSettingsRepository
+                .intSetting(
+                    name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
+                )
+                .map { it == 1 }
+                .flowOn(backgroundDispatcher)
+                .stateIn(scope = scope)
+        }
+    }
 
     suspend fun setShowNotificationsOnLockscreenEnabled(enabled: Boolean) {
         withContext(backgroundDispatcher) {
-            secureSettingsRepository.setInt(
-                name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
-                value = if (enabled) 1 else 0,
+            val peekDisplayNotification = secureSettingsRepository.getInt(
+                name = "peek_display_notifications",
+                defaultValue = 0
             )
+            if (peekDisplayNotification == 1) {
+                secureSettingsRepository.setInt(
+                    name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
+                    value = 0
+                )
+            } else {
+                secureSettingsRepository.setInt(
+                    name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
+                    value = if (enabled) 1 else 0,
+                )
+            }
         }
     }
 }

RisingOS-Revived/android_frameworks_base-new

packages/SystemUI/customization/src/com/android/systemui/shared/notifications/data/repository/NotificationSettingsRepository.kt
@@ -24,6 +24,7 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flowOn
@@ -45,21 +46,42 @@ class NotificationSettingsRepository(
             .distinctUntilChanged()
 
     /** The current state of the notification setting. */
-    suspend fun isShowNotificationsOnLockScreenEnabled(): StateFlow<Boolean> =
-        secureSettingsRepository
-            .intSetting(
-                name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
-            )
-            .map { it == 1 }
-            .flowOn(backgroundDispatcher)
-            .stateIn(scope = backgroundScope)
+    suspend fun isShowNotificationsOnLockScreenEnabled(): StateFlow<Boolean> {
+        val peekDisplayNotification = secureSettingsRepository.getInt(
+            name = "peek_display_notifications",
+            defaultValue = 0
+        )
+
+        return if (peekDisplayNotification == 1) {
+            MutableStateFlow(false)
+        } else {
+            secureSettingsRepository
+                .intSetting(
+                    name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
+                )
+                .map { it == 1 }
+                .flowOn(backgroundDispatcher)
+                .stateIn(scope = backgroundScope)
+        }
+    }
 
     suspend fun setShowNotificationsOnLockscreenEnabled(enabled: Boolean) {
         withContext(backgroundDispatcher) {
-            secureSettingsRepository.setInt(
-                name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
-                value = if (enabled) 1 else 0,
+            val peekDisplayNotification = secureSettingsRepository.getInt(
+                name = "peek_display_notifications",
+                defaultValue = 0
             )
+            if (peekDisplayNotification == 1) {
+                secureSettingsRepository.setInt(
+                    name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
+                    value = 0
+                )
+            } else {
+                secureSettingsRepository.setInt(
+                    name = Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS,
+                    value = if (enabled) 1 else 0,
+                )
+            }
         }
     }
 