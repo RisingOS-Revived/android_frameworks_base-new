5e1ae18027846ca9396be86dd9da314dc9c82ed3
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-27 21:15:27 +0800

    GameManagerService: fix game cpu affinity
    based from: https://github.com/kerneltoast/android_kernel_google_gs201/commit/c8963754beec151ff71bc4460fc078e9ecbb7e9a
    
    Change-Id: I589cdd3e290b6efc64cd2fc08e60180763acfad4
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/app/GameManagerService.java
+++ services/core/java/com/android/server/app/GameManagerService.java
@@ -2393,12 +2393,11 @@ public final class GameManagerService extends IGameManagerService.Stub {
                     continue;
                 }
 
-                Process.setThreadAffinity(pid, boosted ? 0 : 2);
+                Process.setThreadAffinity(pid, 2);
                 Slog.v(TAG, "Set affinity for PID=" + pid + " (Package: " + packageName + 
-                        ", UID=" + uid + ") to " + (boosted ? "big cores (boosted)" : "all cores (normal)"));
+                        ", UID=" + uid + ") to all cores");
             }
         }
-
         private Map<String, Integer> getRunningGamePidsPerPackage(int targetUid) {
             Map<String, Integer> packagePidMap = new HashMap<>();
             ActivityManager am = (ActivityManager) mContext.getSystemService(Context.ACTIVITY_SERVICE);
