crdroidandroid/android_frameworks_base

SystemUI: DynamicBar: Re-add notification and call event
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/compose/AxDynamicBarKeyguardChip.kt
@@ -193,7 +193,7 @@ fun AxDynamicBarKeyguardChip(
         ) {
             val chipState = state
             if (chipState != null) {
-                val displayEvent = chipState.event
+                val displayEvent = chipState.notificationAlert ?: chipState.event
 
                 AnimatedContent(
                     targetState = displayEvent,
@@ -747,6 +747,10 @@ private fun KeyguardPrimaryText(event: IslandEvent, color: Color, modifier: Modi
             )
         }
         is IslandEvent.Alarm -> MarqueeText(event.label.ifEmpty { stringResource(R.string.ax_dynamic_bar_alarm) }, color, modifier)
+        is IslandEvent.Call -> {
+                if (event.callStartTimeMs > 0) CallTimerText(event, modifier, color)
+                else MarqueeText(event.callType ?: stringResource(R.string.ax_dynamic_bar_call), color, modifier)
+        }
         is IslandEvent.Torch -> MarqueeText(
             if (event.supportsLevel) "${(event.level.toFloat() / event.maxLevel * 100).toInt()}%"
             else stringResource(R.string.ax_dynamic_bar_flashlight),
@@ -791,6 +795,7 @@ private fun secondaryTextFor(event: IslandEvent): String? = when (event) {
             "%d:%02d".format(cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE))
         } else null
     }
+    is IslandEvent.Call -> event.callerName
     is IslandEvent.Vpn -> stringResource(R.string.ax_dynamic_bar_active)
     is IslandEvent.BiometricUnlock -> event.sourceName
     is IslandEvent.AppSwitch -> null
@@ -804,6 +809,27 @@ private fun secondaryTextFor(event: IslandEvent): String? = when (event) {
         else -> null
     }
     else -> null
+}
+
+
+@Composable
+private fun CallTimerText(event: IslandEvent.Call, modifier: Modifier, overrideColor: Color? = null) {
+    val isActive = event.callType == "Phone:active"
+    if (isActive) {
+        var elapsedMs by remember(event.callStartTimeMs) {
+            mutableLongStateOf((System.currentTimeMillis() - event.callStartTimeMs).coerceAtLeast(0L))
+        }
+        LaunchedEffect(event.callStartTimeMs) {
+            while (true) {
+                delay(1000)
+                elapsedMs = (System.currentTimeMillis() - event.callStartTimeMs).coerceAtLeast(0L)
+            }
+        }
+        val color = overrideColor ?: GreenAccent
+        Text(formatElapsedTime(elapsedMs), color = color, style = PillMono, modifier = modifier)
+    } else {
+        MarqueeText(stringResource(R.string.ax_dynamic_bar_incoming_call), overrideColor ?: BlueAccent, modifier)
+    }
 }
 
 @Composable

RisingOS-Revived/android_frameworks_base-new

Signed-off-by: Ghosuto <clash.raja10@gmail.com>
Signed-off-by: TwistedVision518 <pranavpraveen0@gmail.com>

packages/SystemUI/src/com/android/systemui/axdynamicbar/ui/compose/AxDynamicBarKeyguardChip.kt
@@ -261,7 +261,7 @@ fun AxDynamicBarKeyguardChip(
         ) {
             val chipState = state
             if (chipState != null) {
-                val rawEvent = chipState.event
+                val rawEvent = chipState.notificationAlert ?: chipState.event
                 val displayEvent: com.android.systemui.axdynamicbar.model.IslandEvent? =
                     when {
                         isKeyguardMusicPillEnabled -> rawEvent
@@ -890,6 +890,10 @@ private fun KeyguardPrimaryText(event: IslandEvent, color: Color, modifier: Modi
             )
         }
         is IslandEvent.Alarm -> MarqueeText(event.label.ifEmpty { stringResource(R.string.ax_dynamic_bar_alarm) }, color, modifier)
+        is IslandEvent.Call -> {
+                if (event.callStartTimeMs > 0) CallTimerText(event, modifier, color)
+                else MarqueeText(event.callType ?: stringResource(R.string.ax_dynamic_bar_call), color, modifier)
+        }
         is IslandEvent.Torch -> MarqueeText(
             if (event.supportsLevel) "${(event.level.toFloat() / event.maxLevel * 100).toInt()}%"
             else stringResource(R.string.ax_dynamic_bar_flashlight),
@@ -1012,6 +1016,7 @@ private fun secondaryTextFor(event: IslandEvent): String? = when (event) {
             triggerTime.takeIfDistinctFrom(event.label)
         } else null
     }
+    is IslandEvent.Call -> event.callerName
     is IslandEvent.BiometricUnlock -> event.sourceName
     is IslandEvent.Notification ->
         event.text
@@ -1038,6 +1043,26 @@ private fun String.takeIfDistinctFrom(vararg others: String?): String? {
     if (value.isEmpty()) return null
     return value.takeIf { candidate ->
         others.none { other -> candidate.equals(other?.trim(), ignoreCase = true) }
+    }
+}
+
+@Composable
+private fun CallTimerText(event: IslandEvent.Call, modifier: Modifier, overrideColor: Color? = null) {
+    val isActive = event.callType == "Phone:active"
+    if (isActive) {
+        var elapsedMs by remember(event.callStartTimeMs) {
+            mutableLongStateOf((System.currentTimeMillis() - event.callStartTimeMs).coerceAtLeast(0L))
+        }
+        LaunchedEffect(event.callStartTimeMs) {
+            while (true) {
+                delay(1000)
+                elapsedMs = (System.currentTimeMillis() - event.callStartTimeMs).coerceAtLeast(0L)
+            }
+        }
+        val color = overrideColor ?: GreenAccent
+        Text(formatElapsedTime(elapsedMs), color = color, style = PillMono, modifier = modifier)
+    } else {
+        MarqueeText(stringResource(R.string.ax_dynamic_bar_incoming_call), overrideColor ?: BlueAccent, modifier)
     }
 }
 