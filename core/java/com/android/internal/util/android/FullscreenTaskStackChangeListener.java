1xtAsh/cr_base

SystemUI: Implement RefreshRateManager [1/2]
Includes:
- Per-app refresh rate config
- Extreme refresh rate (Force all apps to run in maximum refresh rate)
- Temp refresh rate allowed (Used in MEMC)
- Refresh rate QS tile

Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
[nurkeinneid: move to fwb and drop sdk extensions/dependencies]
Co-authored-by: NurKeinNeid <nurkeinneid@derpfest.org>
Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

core/java/com/android/internal/util/derpfest/FullscreenTaskStackChangeListener.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.derpfest;
+
+import static android.app.ActivityTaskManager.INVALID_TASK_ID;
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.IActivityTaskManager;
+import android.app.TaskStackListener;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * TaskStackListener that ignores freeform / mini-window focuses change
+ *
+ * @hide
+ */
+public abstract class FullscreenTaskStackChangeListener extends TaskStackListener {
+
+    private static final String DEFAULT_TAG = "FullscreenTaskStackChangeListener";
+
+    private final ActivityManager mActivityManager;
+    private final ActivityTaskManager mActivityTaskManager;
+    private final IActivityTaskManager mIActivityTaskManager;
+
+    private final boolean mObserveActivityChange;
+
+    private boolean mDebug = false;
+    private String mDebugTag = DEFAULT_TAG;
+
+    private boolean mListening = false;
+
+    private String mTopPackage = "";
+    private String mTopActivity = "";
+    private int mTopTaskId = INVALID_TASK_ID;
+
+    public FullscreenTaskStackChangeListener(Context context) {
+        this(context, false);
+    }
+
+    public FullscreenTaskStackChangeListener(Context context, boolean observeActivity) {
+        mActivityManager = context.getSystemService(ActivityManager.class);
+        mActivityTaskManager = ActivityTaskManager.getInstance();
+        mIActivityTaskManager = ActivityTaskManager.getService();
+        mObserveActivityChange = observeActivity;
+    }
+
+    public void setDebug(boolean debug) {
+        mDebug = debug;
+    }
+
+    public void setDebugTag(String tag) {
+        mDebugTag = tag;
+    }
+
+    public void setListening(boolean listening) {
+        if (mListening != listening) {
+            if (listening) {
+                mActivityTaskManager.registerTaskStackListener(this);
+                mListening = true;
+            } else {
+                mActivityTaskManager.unregisterTaskStackListener(this);
+                mListening = false;
+            }
+        }
+    }
+
+    private void handleChange(String newPackageName, String newActivityName, int newTaskId) {
+        if (mTopPackage.equals(newPackageName) && mTopActivity.equals(newActivityName)) {
+            return;
+        }
+        mTopActivity = newActivityName;
+        if (!mObserveActivityChange && mTopPackage.equals(newPackageName)) {
+            return;
+        }
+        mTopPackage = newPackageName;
+        mTopTaskId = newTaskId;
+        if (mDebug) {
+            Log.d(mDebugTag, "Change: mTopPackage=" + mTopPackage
+                    + ", mTopActivity=" + mTopActivity
+                    + ", mTopTaskId=" + mTopTaskId);
+        }
+        onFullscreenTaskChanged(mTopPackage, mTopActivity, mTopTaskId);
+    }
+
+    @Override
+    public void onTaskStackChanged() {
+        forceCheck();
+    }
+
+    @Override
+    public void onTaskFocusChanged(int taskId, boolean focused) {
+        if (focused) {
+            forceCheck();
+        }
+    }
+
+    public void forceCheck() {
+        try {
+            final ActivityTaskManager.RootTaskInfo info = mIActivityTaskManager.getFocusedRootTaskInfo();
+            if (info == null) {
+                return;
+            }
+            if (info.getWindowingMode() == WINDOWING_MODE_FREEFORM) {
+                return;
+            }
+            final ComponentName topActivity = info.topActivity;
+            if (topActivity == null) {
+                return;
+            }
+            handleChange(topActivity.getPackageName(), topActivity.getClassName(), info.taskId);
+        } catch (RemoteException e) {}
+    }
+
+    public String getTopPackageName() {
+        return mTopPackage;
+    }
+
+    public String getTopActivityName() {
+        return mTopActivity;
+    }
+
+    public int getTopTaskId() {
+        return mTopTaskId;
+    }
+
+    public abstract void onFullscreenTaskChanged(
+            String packageName, String activityName, int taskId);
+}

core/java/org/derpfest/display/RefreshRateManager.java
@@ -1,0 +1,173 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.display;
+
+import android.annotation.SystemService;
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Slog;
+
+import com.android.internal.util.derp.derpUtils;
+import com.android.internal.util.derpfest.DisplayRefreshRateHelper;
+
+/** @hide */
+@SystemService(Context.REFRESH_RATE_MANAGER_SERVICE)
+public class RefreshRateManager {
+
+    private static final String TAG = "RefreshRateManager";
+
+    private final Context mContext;
+    private final DisplayRefreshRateHelper mDisplayRefreshRateHelper;
+    private final IRefreshRateManagerService mService;
+
+    public RefreshRateManager(Context context, IRefreshRateManagerService service) {
+        mContext = context;
+        mService = service;
+        mDisplayRefreshRateHelper = DisplayRefreshRateHelper.getInstance(context);
+    }
+
+    public void requestMemcRefreshRate(int refreshRate) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to request memc refresh rate. Service is null");
+            return;
+        }
+        if (refreshRate > 0f &&
+                !mDisplayRefreshRateHelper.isRefreshRateValid(refreshRate)) {
+            Slog.e(TAG, "Failed to request memc refresh rate. Invalid refresh rate");
+            return;
+        }
+        try {
+            mService.requestMemcRefreshRate(refreshRate);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void clearRequestedMemcRefreshRate() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to clear memc refresh rate. Service is null");
+            return;
+        }
+        try {
+            mService.clearRequestedMemcRefreshRate();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public int getRefreshRateForPackage(String packageName) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get refresh rate for package. Service is null");
+            return -1;
+        }
+        if (!derpUtils.isPackageInstalled(mContext, packageName)) {
+            Slog.e(TAG, "Failed to get refresh rate for package. Package " + packageName + " is unavailable");
+            return -1;
+        }
+        try {
+            return mService.getRefreshRateForPackage(packageName);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void setRefreshRateForPackage(String packageName, int refreshRate) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to set refresh rate for package. Service is null");
+            return;
+        }
+        if (!derpUtils.isPackageInstalled(mContext, packageName)) {
+            Slog.e(TAG, "Failed to set refresh rate for package. Package " + packageName + " is unavailable");
+            return;
+        }
+        if (refreshRate > 0f &&
+                !mDisplayRefreshRateHelper.isRefreshRateValid(refreshRate)) {
+            Slog.e(TAG, "Failed to set refresh rate for package. Invalid refresh rate");
+            return;
+        }
+        try {
+            mService.setRefreshRateForPackage(packageName, refreshRate);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void unsetRefreshRateForPackage(String packageName) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to unset refresh rate for package. Service is null");
+            return;
+        }
+        if (!derpUtils.isPackageInstalled(mContext, packageName)) {
+            Slog.e(TAG, "Failed to unset refresh rate for package. Package " + packageName + " is unavailable");
+            return;
+        }
+        try {
+            mService.unsetRefreshRateForPackage(packageName);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void setExtremeRefreshRateEnabled(boolean enabled) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to set extreme refresh rate. Service is null");
+            return;
+        }
+        try {
+            mService.setExtremeRefreshRateEnabled(enabled);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public int getRequestedRefreshRate() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get requested refresh rate. Service is null");
+            return -1;
+        }
+        try {
+            return mService.getRequestedRefreshRate();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public int getRequestedMemcRefreshRate() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get requested memc refresh rate. Service is null");
+            return -1;
+        }
+        try {
+            return mService.getRequestedMemcRefreshRate();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public boolean registerRefreshRateListener(IRefreshRateListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to register refresh rate listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.registerRefreshRateListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public boolean unregisterRefreshRateListener(IRefreshRateListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to unregister refresh rate listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.unregisterRefreshRateListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+}

RisingOS-Revived/android_frameworks_base-new

Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

core/java/com/android/internal/util/android/FullscreenTaskStackChangeListener.java
@@ -1,0 +1,136 @@
+/*
+ * Copyright (C) 2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.internal.util.android;
+
+import static android.app.ActivityTaskManager.INVALID_TASK_ID;
+import static android.app.WindowConfiguration.WINDOWING_MODE_FREEFORM;
+
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.IActivityTaskManager;
+import android.app.TaskStackListener;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Log;
+
+/**
+ * TaskStackListener that ignores freeform / mini-window focuses change
+ *
+ * @hide
+ */
+public abstract class FullscreenTaskStackChangeListener extends TaskStackListener {
+
+    private static final String DEFAULT_TAG = "FullscreenTaskStackChangeListener";
+
+    private final ActivityManager mActivityManager;
+    private final ActivityTaskManager mActivityTaskManager;
+    private final IActivityTaskManager mIActivityTaskManager;
+
+    private final boolean mObserveActivityChange;
+
+    private boolean mDebug = false;
+    private String mDebugTag = DEFAULT_TAG;
+
+    private boolean mListening = false;
+
+    private String mTopPackage = "";
+    private String mTopActivity = "";
+    private int mTopTaskId = INVALID_TASK_ID;
+
+    public FullscreenTaskStackChangeListener(Context context) {
+        this(context, false);
+    }
+
+    public FullscreenTaskStackChangeListener(Context context, boolean observeActivity) {
+        mActivityManager = context.getSystemService(ActivityManager.class);
+        mActivityTaskManager = ActivityTaskManager.getInstance();
+        mIActivityTaskManager = ActivityTaskManager.getService();
+        mObserveActivityChange = observeActivity;
+    }
+
+    public void setDebug(boolean debug) {
+        mDebug = debug;
+    }
+
+    public void setDebugTag(String tag) {
+        mDebugTag = tag;
+    }
+
+    public void setListening(boolean listening) {
+        if (mListening != listening) {
+            if (listening) {
+                mActivityTaskManager.registerTaskStackListener(this);
+                mListening = true;
+            } else {
+                mActivityTaskManager.unregisterTaskStackListener(this);
+                mListening = false;
+            }
+        }
+    }
+
+    private void handleChange(String newPackageName, String newActivityName, int newTaskId) {
+        if (mTopPackage.equals(newPackageName) && mTopActivity.equals(newActivityName)) {
+            return;
+        }
+        mTopActivity = newActivityName;
+        if (!mObserveActivityChange && mTopPackage.equals(newPackageName)) {
+            return;
+        }
+        mTopPackage = newPackageName;
+        mTopTaskId = newTaskId;
+        if (mDebug) {
+            Log.d(mDebugTag, "Change: mTopPackage=" + mTopPackage
+                    + ", mTopActivity=" + mTopActivity
+                    + ", mTopTaskId=" + mTopTaskId);
+        }
+        onFullscreenTaskChanged(mTopPackage, mTopActivity, mTopTaskId);
+    }
+
+    @Override
+    public void onTaskStackChanged() {
+        forceCheck();
+    }
+
+    @Override
+    public void onTaskFocusChanged(int taskId, boolean focused) {
+        if (focused) {
+            forceCheck();
+        }
+    }
+
+    public void forceCheck() {
+        try {
+            final ActivityTaskManager.RootTaskInfo info = mIActivityTaskManager.getFocusedRootTaskInfo();
+            if (info == null) {
+                return;
+            }
+            if (info.getWindowingMode() == WINDOWING_MODE_FREEFORM) {
+                return;
+            }
+            final ComponentName topActivity = info.topActivity;
+            if (topActivity == null) {
+                return;
+            }
+            handleChange(topActivity.getPackageName(), topActivity.getClassName(), info.taskId);
+        } catch (RemoteException e) {}
+    }
+
+    public String getTopPackageName() {
+        return mTopPackage;
+    }
+
+    public String getTopActivityName() {
+        return mTopActivity;
+    }
+
+    public int getTopTaskId() {
+        return mTopTaskId;
+    }
+
+    public abstract void onFullscreenTaskChanged(
+            String packageName, String activityName, int taskId);
+}

core/java/org/derpfest/display/RefreshRateManager.java
@@ -1,0 +1,173 @@
+/*
+ * Copyright (C) 2023-2024 The Nameless-AOSP Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.derpfest.display;
+
+import android.annotation.SystemService;
+import android.content.Context;
+import android.os.RemoteException;
+import android.util.Slog;
+
+import com.android.internal.util.android.Utils;
+import com.android.internal.util.android.DisplayRefreshRateHelper;
+
+/** @hide */
+@SystemService(Context.REFRESH_RATE_MANAGER_SERVICE)
+public class RefreshRateManager {
+
+    private static final String TAG = "RefreshRateManager";
+
+    private final Context mContext;
+    private final DisplayRefreshRateHelper mDisplayRefreshRateHelper;
+    private final IRefreshRateManagerService mService;
+
+    public RefreshRateManager(Context context, IRefreshRateManagerService service) {
+        mContext = context;
+        mService = service;
+        mDisplayRefreshRateHelper = DisplayRefreshRateHelper.getInstance(context);
+    }
+
+    public void requestMemcRefreshRate(int refreshRate) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to request memc refresh rate. Service is null");
+            return;
+        }
+        if (refreshRate > 0f &&
+                !mDisplayRefreshRateHelper.isRefreshRateValid(refreshRate)) {
+            Slog.e(TAG, "Failed to request memc refresh rate. Invalid refresh rate");
+            return;
+        }
+        try {
+            mService.requestMemcRefreshRate(refreshRate);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void clearRequestedMemcRefreshRate() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to clear memc refresh rate. Service is null");
+            return;
+        }
+        try {
+            mService.clearRequestedMemcRefreshRate();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public int getRefreshRateForPackage(String packageName) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get refresh rate for package. Service is null");
+            return -1;
+        }
+        if (!Utils.isPackageInstalled(mContext, packageName)) {
+            Slog.e(TAG, "Failed to get refresh rate for package. Package " + packageName + " is unavailable");
+            return -1;
+        }
+        try {
+            return mService.getRefreshRateForPackage(packageName);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void setRefreshRateForPackage(String packageName, int refreshRate) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to set refresh rate for package. Service is null");
+            return;
+        }
+        if (!Utils.isPackageInstalled(mContext, packageName)) {
+            Slog.e(TAG, "Failed to set refresh rate for package. Package " + packageName + " is unavailable");
+            return;
+        }
+        if (refreshRate > 0f &&
+                !mDisplayRefreshRateHelper.isRefreshRateValid(refreshRate)) {
+            Slog.e(TAG, "Failed to set refresh rate for package. Invalid refresh rate");
+            return;
+        }
+        try {
+            mService.setRefreshRateForPackage(packageName, refreshRate);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void unsetRefreshRateForPackage(String packageName) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to unset refresh rate for package. Service is null");
+            return;
+        }
+        if (!Utils.isPackageInstalled(mContext, packageName)) {
+            Slog.e(TAG, "Failed to unset refresh rate for package. Package " + packageName + " is unavailable");
+            return;
+        }
+        try {
+            mService.unsetRefreshRateForPackage(packageName);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public void setExtremeRefreshRateEnabled(boolean enabled) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to set extreme refresh rate. Service is null");
+            return;
+        }
+        try {
+            mService.setExtremeRefreshRateEnabled(enabled);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public int getRequestedRefreshRate() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get requested refresh rate. Service is null");
+            return -1;
+        }
+        try {
+            return mService.getRequestedRefreshRate();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public int getRequestedMemcRefreshRate() {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to get requested memc refresh rate. Service is null");
+            return -1;
+        }
+        try {
+            return mService.getRequestedMemcRefreshRate();
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public boolean registerRefreshRateListener(IRefreshRateListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to register refresh rate listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.registerRefreshRateListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    public boolean unregisterRefreshRateListener(IRefreshRateListener.Stub listener) {
+        if (mService == null) {
+            Slog.e(TAG, "Failed to unregister refresh rate listener. Service is null");
+            return false;
+        }
+        try {
+            return mService.unregisterRefreshRateListener(listener);
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+}